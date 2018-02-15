b6d2f1a7f23ff31fdf7e38be340cf6a93adf5b5b
pdenette <pdenette@tampabay.rr.com>
2018-02-15 16:54:22 -0500

    MainStructure
    Added inital function buckets and working on file location input string.

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 "use strict";
 
 (function main() {
-
-    let helloPhil = "Hello, Phil!";
-    let helloall = 'lkjaffsdf'
-
-    console.log(helloPhil, helloall);
+    promptUser();
 })();
+
+function readFile() {
+
+}
+
+function parseLine() {
+
+}
+
+function promptUser() {
+    let readline = require('readline');
+    let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    let answer;
+
+    rl.question("What do you think of Node.js? ", function(input) {
+        answer = input;
+        rl.close();
+    });
+
+}
