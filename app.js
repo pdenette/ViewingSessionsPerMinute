a1230d94aa1353df0cb6a19ebce4ca1e7f8f211c
pdenette <pdenette@tampabay.rr.com>
2018-02-16 15:09:38 -0500

    Parsingfile
    Put in file read and parse out time for log file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 
 let convict = require('convict');
-
 // Initialize Convict to allow filepath command line parameter
 let config = convict({
   env: {
@@ -23,12 +22,41 @@ let config = convict({
 // Perform validation
 config.validate({allowed: 'strict'});
 
-//
+// Read Lines from PLBSepLogs
+let fs = require('fs');
+let stream = fs.createReadStream(config.get("logfilepath") +"plb.sep_single.log", {flags: 'r', encoding: 'utf-8'});
+let buf = '';
+stream.on('data', function(d) {
+  buf += d.toString(); // when data is read, stash it in a string buffer
+  pump(); // then process the buffer
+});
 
-function readFile() {
+function pump() {
+  let pos;
 
+  while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+      if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
+          buf = buf.slice(1); // discard it
+          continue; // so that the next iteration will start with data
+      }
+      
+      let line = buf.slice(0,pos);
 
+      if (RegExp("reqBegin.+?(?=POST).+?ViewingSession\"").test(line)) {
+        processLine(line); // hand off the line
+      }
+
+      buf = buf.slice(pos+1); // and slice the processed data off the buffer
+  }
 }
+
+function processLine(line) { // here's where we do something with a line
+
+        let SHITINEED=JSON.parse(line).time; // do something with the data here!
+        console.log(SHITINEED);
+}
+
+//
 
 function parseLine() {
 
