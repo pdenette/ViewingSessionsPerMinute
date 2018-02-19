27c79b3a0617ed242c7e7cb666993584d1e5bbd0
pdenette <pdenette@tampabay.rr.com>
2018-02-19 12:41:33 -0500

    Added array and consolidated
    Added a new array to hold the date and time stamps of the viewing
    sessions and consolidated mulitple functions into main.

--- app.js
+++ app.js
@@ -1,72 +1,55 @@
 "use strict";
 
 let convict = require('convict');
-// Initialize Convict to allow filepath command line parameter
 let config = convict({
-  env: {
-    doc: "The application environment.",
-    format: ["production", "development", "test"],
-    default: "development",
-    env: "NODE_ENV",
-    arg: "env"
-  },
-  logfilepath: {
-    doc: "The PrizmDoc log filepath.",
-    format: "*",
-    default: "",
-    env: "LOG_FILEPATH",
-    arg: "logfilepath"
-  }
+	env: {
+		doc: "The application environment.",
+		format: ["production", "development", "test"],
+		default: "development",
+		env: "NODE_ENV",
+		arg: "env"
+	},
+	logfilepath: {
+		doc: "The PrizmDoc log filepath.",
+		format: "*",
+		default: "",
+		env: "LOG_FILEPATH",
+		arg: "logfilepath"
+	}
 });
 
 // Perform validation
-config.validate({allowed: 'strict'});
+config.validate({ allowed: 'strict' });
 
-// Read Lines from PLBSepLogs
-let fs = require('fs');
-let stream = fs.createReadStream(config.get("logfilepath") +"plb.sep_single.log", {flags: 'r', encoding: 'utf-8'});
-let buf = '';
-stream.on('data', function(d) {
-  buf += d.toString(); // when data is read, stash it in a string buffer
-  pump(); // then process the buffer
-});
-
-function pump() {
-  let pos;
-
-  while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
-      if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
-          buf = buf.slice(1); // discard it
-          continue; // so that the next iteration will start with data
-      }
-      
-      let line = buf.slice(0,pos);
-
-      if (RegExp("reqBegin.+?(?=POST).+?ViewingSession\"").test(line)) {
-        processLine(line); // hand off the line
-      }
-
-      buf = buf.slice(pos+1); // and slice the processed data off the buffer
-  }
-}
-
-function processLine(line) { // here's where we do something with a line
-
-        let SHITINEED=JSON.parse(line).time; // do something with the data here!
-        console.log(SHITINEED);
-}
-
-//
-
-function parseLine() {
-
-}
-
+// main()
 (function main() {
-    if (config.get("logfilepath") === "") {
-        console.error("No filepath specified.");
-        process.exit(1);
-    }
+	if (config.get("logfilepath") === "") {
+		console.error("No filepath specified.");
+		process.exit(1);
+	}
+	let viewingsessiondatetimearray = [];
+	let fs = require('fs');
+	let stream = fs.createReadStream(config.get("logfilepath") + "plb.sep_single.log", { flags: 'r', encoding: 'utf-8' });
+	let buf = '';
 
-    console.log(config.get("logfilepath"));
+	stream.on('data', function (d) {
+		buf += d.toString(); // when data is read, stash it in a string buffer
+		let pos;
+		while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+			if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
+				buf = buf.slice(1); // discard it
+				continue; // so that the next iteration will start with data
+			}
+			let line = buf.slice(0, pos);
+			if (RegExp("reqBegin.+?(?=POST).+?ViewingSession\"").test(line)) {
+				let viewingsessiondatetime = JSON.parse(line).time; // do something with the data here!
+				viewingsessiondatetimearray.push(line);
+				console.log(viewingsessiondatetime);
+			}
+			buf = buf.slice(pos + 1); // and slice the processed data off the buffer
+		}
+	});
+	stream.on("finish", function () {
+		stream.close();
+	})
 })();
