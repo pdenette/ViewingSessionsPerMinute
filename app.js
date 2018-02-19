fa9f9c5bcb1e950e6a83cb1a27fad18cc39205e0
pdenette <pdenette@tampabay.rr.com>
2018-02-19 16:31:02 -0500

    BrianStreamlined
    removed stream and while loops made it better

--- app.js
+++ app.js
@@ -27,34 +27,25 @@ config.validate({ allowed: 'strict' });
 		console.error("No filepath specified.");
 		process.exit(1);
 	}
-	let viewingsessiondatetimearray = [];
-	let fs = require('fs');
-	let stream = fs.createReadStream(config.get("logfilepath") + "plb.sep_single.log", { flags: 'r', encoding: 'utf-8' });
-	let buf = '';
 
-	stream.on('data', function (d) {
-		buf += d.toString(); // when data is read, stash it in a string buffer
-		let pos;
-		while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
-			if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
-				buf = buf.slice(1); // discard it
-				continue; // so that the next iteration will start with data
-			}
-			let line = buf.slice(0, pos);
-			if (RegExp("reqBegin.+?(?=POST).+?ViewingSession\"").test(line)) {
-				let viewingsessiondatetime = JSON.parse(line).time; // do something with the data here!
-				viewingsessiondatetimearray.push(viewingsessiondatetime);
-			}
-			buf = buf.slice(pos + 1); // and slice the processed data off the buffer
+	let readline = require('readline').createInterface({
+		input: require('fs').createReadStream(config.get("logfilepath") + "plb.sep_single.log")
+	});
+
+	let lines = [];
+
+	readline.on("line", function (line) {
+		if (RegExp("reqBegin.+?(?=POST).+?ViewingSession\"").test(line)) {
+			lines.push(JSON.parse(line).time);
 		}
 	});
 
-	stream.on("end", function () {
-		stream.close();
-		let moment = require("moment")
-		console.log(viewingsessiondatetimearray[0]);
-		console.log(viewingsessiondatetimearray[viewingsessiondatetimearray.length - 1]);
-		let totalminutes = (viewingsessiondatetimearray[viewingsessiondatetimearray.length - 1]) - (viewingsessiondatetimearray[0]);
+	readline.on("close", function () {
+		// console.log(lines.join("\n"));
+		// let moment = require("moment")
+		console.log(lines[0]);
+		console.log(lines[lines.length - 1]);
+		let totalminutes = (lines[lines.length - 1]) - (lines[0]);
 		console.log(totalminutes);
 	})
 })();
