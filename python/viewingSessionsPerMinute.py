cd9082b14d1d6bea971e53b88dc5109d1e9a560a
pdenette <pdenette@tampabay.rr.com>
2018-02-21 14:30:16 -0500

    updatedpythoncode

--- python/viewingSessionsPerMinute.py
+++ python/viewingSessionsPerMinute.py
@@ -1,35 +1,23 @@
 def main():
-    logfiledirectory = filelocation()
-    print(logfiledirectory)
-
-def filelocation():
-    logfiledirectory = input ('Please Enter the location of the log file: ')
-    
-    return logfiledirectory
+    logFileDirectory = getLogFileLocation()
+    parseLogFile(logFileDirectory)
 
 
+def getLogFileLocation():
+    logFileDirectory = input('Please Enter the location of the log file: ')
+
+    return logFileDirectory
 
 
+def parseLogFile(logFileDirectory):
+    print(logFileDirectory+'plb.sep_single.log')
+    f = open(logFileDirectory+'plb.sep_single.log')
+    while True:
+        text = f.readline()
+        # if re.match(r'reqBegin.+?(?=POST).+?ViewingSession\"', text) in text:
+            print(text)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# this stays at the bottom
+        # this stays at the bottom
 if __name__ == "__main__":
     main()
