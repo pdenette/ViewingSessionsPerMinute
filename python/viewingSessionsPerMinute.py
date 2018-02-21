f8355685f4c25a7fe91407c03073c0dad0de4eeb
pdenette <pdenette@tampabay.rr.com>
2018-02-21 13:00:11 -0500

    merged python and node into 1 repo

---
+++ python/viewingSessionsPerMinute.py
@@ -0,0 +1,35 @@
+def main():
+    logfiledirectory = filelocation()
+    print(logfiledirectory)
+
+def filelocation():
+    logfiledirectory = input ('Please Enter the location of the log file: ')
+    
+    return logfiledirectory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# this stays at the bottom
+if __name__ == "__main__":
+    main()
