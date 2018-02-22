ebf39c1e06807a7ae7cefaf3d91a51898a060193
pdenette <pdenette@tampabay.rr.com>
2018-02-22 16:42:43 -0500

    FinalizedPython
    Finished Python

--- python/viewingSessionsPerMinute.py
+++ python/viewingSessionsPerMinute.py
@@ -1,3 +1,9 @@
+import json
+import time
+from calendar import timegm
+from datetime import datetime
+
+
 def main():
     logFileDirectory = getLogFileLocation()
     parseLogFile(logFileDirectory)
@@ -10,14 +16,30 @@ def getLogFileLocation():
 
 
 def parseLogFile(logFileDirectory):
-    print(logFileDirectory+'plb.sep_single.log')
-    f = open(logFileDirectory+'plb.sep_single.log')
-    while True:
-        text = f.readline()
-        # if re.match(r'reqBegin.+?(?=POST).+?ViewingSession\"', text) in text:
-            print(text)
+    vsdatearray = []
+    with open(logFileDirectory + '\plb.sep_single.log', 'r') as fp:
+        for line in fp:
+            data = json.loads(line)
+            if 'req' in data and 'url' in data['req'] and data['req']['url'] == 'http://127.0.0.1:19000/PCCIS/V1/ViewingSession':
+                vsdatearray.append(data['time'])
+        totalViewingsessionCount = len(vsdatearray)
+        firstPostDate = datetime.strptime(
+            vsdatearray[0].replace('T', ' '),
+            '%Y-%m-%d %H:%M:%S.%fZ'
+        )
+        lastPostDate = datetime.strptime(
+            vsdatearray[len(vsdatearray)-1].replace('T', ' '),
+            '%Y-%m-%d %H:%M:%S.%fZ'
+        )
+        dateDifferenceInMinutes = ((time.mktime(lastPostDate.timetuple()) -
+                                    time.mktime(firstPostDate.timetuple()))/60)
+        print("Total Number of Viewing Sessions Created between ",
+              firstPostDate, " and ", lastPostDate, " is ", totalViewingsessionCount)
+        print("The number of viewing sessiond per minute calculated is ",
+              totalViewingsessionCount / dateDifferenceInMinutes)
 
 
-        # this stays at the bottom
+
+    # this stays at the bottom
 if __name__ == "__main__":
     main()
